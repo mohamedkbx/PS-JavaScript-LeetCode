91a37d9d1d76c9e629211e30f5ad26249f11413c
Mohamed Khaled <109092116+mohamedkbx@users.noreply.github.com>
2025-01-06 22:01:48 +0200

    [977] [Time Beats: 91.68%] [Memory Beats: 84.33%] - LeetPush

--- Arrays/977-Squares-of-a-Sorted-Array.js
+++ Arrays/977-Squares-of-a-Sorted-Array.js
@@ -2,10 +2,21 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortedSquares = function(nums) {
-    let newArr = nums.map((number)=>{
-        return number ** 2
+var sortedSquares = function (nums) {
+
+    let newArr = new Array(nums.length).fill(0)
+    let left = 0
+    let right = nums.length - 1
+    for (let i = nums.length - 1; i >= 0; i--) {
+        let leftSquared = Math.pow(nums[left], 2)
+        let rightSquared = Math.pow(nums[right], 2)
+        if (leftSquared > rightSquared) {
+            newArr[i] = leftSquared;
+            left++
+        } else {
+            newArr[i] = rightSquared;
+            right--;
+        }
     }
-    ).sort((a,b)=>a-b)
-    return newArr
+     return newArr
 };
